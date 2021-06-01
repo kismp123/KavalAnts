e7555e43560fa4410a8fde205f91bd45495da107
gegul <kismp123@gmail.com>
2021-06-01 20:10:20 +0900

    Create CoverageAll.py

---
+++ 2.Coverage/CoverageAll.py
@@ -0,0 +1,14 @@
+import os
+from subprocess import call
+
+python = 'C:\\Python27\\python.exe'
+codeCov = '..\\2.Coverage\\Coverage.py' 
+inputDir = ".\\"
+exePath = "pathToExe\\notepad.exe"
+
+for root, subFolder, files in os.walk(inputDir):
+	for item in files:
+		fname = os.path.join(inputDir, root, item)
+        if( fname.endswith('.pdf') or True ):
+			print "Coverage... '%s'" % fname
+			call([python, codeCov, exePath, fname])
