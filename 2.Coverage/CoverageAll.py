f1d1abdd7978f07c542e2f912b4b584297bd6bfc
gegul <kismp123@gmail.com>
2021-06-01 20:45:40 +0900

    Update CoverageAll.py

--- 2.Coverage/CoverageAll.py
+++ 2.Coverage/CoverageAll.py
@@ -9,6 +9,5 @@ exePath = "pathToExe\\notepad.exe"
 for root, subFolder, files in os.walk(inputDir):
 	for item in files:
 		fname = os.path.join(inputDir, root, item)     
-        if( fname.endswith('pdf')):
-			print "Coverage... '%s'" % fname
-			call([python, codeCov, exePath, fname])
+        print "Coverage... '%s'" % fname
+        call([python, codeCov, exePath, fname])
